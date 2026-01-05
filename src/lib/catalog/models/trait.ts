117a5f20bbd1bf97873aad5bf0dae53ce6c88bb8
marticongost <marticongost@gmail.com>
2026-01-05 19:04:06 +0100

    Fix Trait#archetype for nested archetypes

--- src/lib/catalog/models/trait.ts
+++ src/lib/catalog/models/trait.ts
@@ -18,6 +18,7 @@ export class Trait {
 	readonly properties: Array<Property>;
 	readonly capabilities: Array<Capability>;
 	private _archetype: Trait | undefined | typeof NOT_COMPUTED;
+	private _isArchetype: boolean | typeof NOT_COMPUTED;
 	private _subtraits: Array<Trait> | typeof NOT_COMPUTED;
 
 	constructor({ title, description, properties, capabilities }: TraitProps) {
@@ -26,6 +27,7 @@ export class Trait {
 		this.properties = properties ?? [];
 		this.capabilities = capabilities ?? [];
 		this._archetype = NOT_COMPUTED;
+		this._isArchetype = NOT_COMPUTED;
 		this._subtraits = NOT_COMPUTED;
 	}
 
@@ -33,17 +35,27 @@ export class Trait {
 		return this.archetype === undefined;
 	}
 
+	get isArchetype(): boolean {
+		if (this._isArchetype === NOT_COMPUTED) {
+			const metadata = getEntryMetadata(this);
+			this._isArchetype =
+				metadata.path.length >= 2 &&
+				metadata.path[metadata.path.length - 1] === metadata.path[metadata.path.length - 2];
+		}
+		return this._isArchetype;
+	}
+
 	get archetype(): Trait | undefined {
 		if (this._archetype === NOT_COMPUTED) {
 			const metadata = getEntryMetadata(this);
-			if (
-				metadata.path.length < 2 ||
-				metadata.path[metadata.path.length - 1] === metadata.path[metadata.path.length - 2]
-			) {
+			let i = metadata.path.length - 2;
+			if (this.isArchetype) {
+				i--;
+			}
+			if (i < 0) {
 				this._archetype = undefined;
 			} else {
-				const archetypeId = metadata.path[metadata.path.length - 2];
-				this._archetype = metadata.catalog.require(archetypeId);
+				this._archetype = metadata.catalog.require(metadata.path[i]);
 			}
 		}
 		return this._archetype;
