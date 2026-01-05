f766fe2caf9b50b0075ddd9b5547c6f5aabad0b5
marticongost <marticongost@gmail.com>
2026-01-05 19:10:35 +0100

    Make text return a text node instead of a span

---
+++ src/lib/components/localisation/Text.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { getLocale } from "$lib/context/locale";
+
+    export let ca: string = "";
+    export let es: string = "";
+    export let en: string = "";
+
+    const locale = getLocale();
+</script>
+
+{#if locale === 'ca'}
+    {ca}
+{:else if locale === 'es'}
+    {es}
+{:else}
+    {en}
+{/if}
