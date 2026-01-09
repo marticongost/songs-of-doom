de21e48b3eae5401e56cd4dc88a0bbdabe0a6732
marticongost <marticongost@gmail.com>
2026-01-09 18:55:48 +0100

    Add the wooden-shield card

---
+++ src/lib/catalog/data/items/shields/wooden-shield.ts
@@ -0,0 +1,24 @@
+import { fullyRechargeOnChapterStart } from '$lib/catalog/models/common';
+import { DefendEffect } from '$lib/catalog/models/effects';
+import { Item } from '$lib/catalog/models/inventory';
+import { Reaction } from '$lib/catalog/models/reaction';
+import shield from '../../properties/shield';
+
+export default new Item({
+	title: { ca: 'Escut de fusta', es: 'Escudo de madera', en: 'Wooden Shield' },
+	slot: 'hand',
+	properties: [shield],
+	maxCharges: 2,
+	capabilities: [
+		fullyRechargeOnChapterStart,
+		new Reaction({
+			triggers: ['receivingAttack'],
+			cost: { charges: 1, agility: 1 },
+			effects: [
+				new DefendEffect({
+					expression: 'agility+2'
+				})
+			]
+		})
+	]
+});
