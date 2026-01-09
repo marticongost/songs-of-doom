68cc036d6ad10a16b40512b7063cd89abf440de8
marticongost <marticongost@gmail.com>
2026-01-09 21:18:58 +0100

    Make Adventurer a standard trait

---
+++ src/lib/catalog/data/properties/standard.ts
@@ -0,0 +1,10 @@
+import { Keyword } from '$lib/catalog/models/properties';
+
+export default new Keyword({
+	title: { ca: 'Estàndard', es: 'Estándar', en: 'Standard' },
+	description: {
+		ca: "S'aplica a trets que són comuns a tots els jugadors, sense haver-los d'aquirir.",
+		es: 'Se aplica a rasgos que son comunes a todos los jugadores, sin tener que adquirirlos.',
+		en: "Applies to traits that are common to all players and don't have to be bought."
+	}
+});
--- src/lib/catalog/data/traits/adventurer/adventurer.ts
+++ src/lib/catalog/data/traits/adventurer/adventurer.ts
@@ -1,9 +1,23 @@
+import { DefendEffect } from '$lib/catalog/models/effects';
+import { Reaction } from '$lib/catalog/models/reaction';
 import { Trait } from '$lib/catalog/models/trait';
+import standard from '../../properties/standard';
 
 export default new Trait({
 	title: {
 		ca: 'Aventurer',
 		es: 'Aventurero',
 		en: 'Adventurer'
-	}
+	},
+	properties: [standard],
+	capabilities: [
+		new Reaction({
+			triggers: ['receivingAttack'],
+			effects: [
+				new DefendEffect({
+					expression: 'agility-1'
+				})
+			]
+		})
+	]
 });
