b5d26c9504f29271d0d610f876124d99e09598b1
marticongost <marticongost@gmail.com>
2026-01-10 20:01:12 +0100

    Add a basic attack to Adventurer

--- src/lib/catalog/data/traits/adventurer/adventurer.ts
+++ src/lib/catalog/data/traits/adventurer/adventurer.ts
@@ -1,4 +1,5 @@
-import { DefendEffect } from '$lib/catalog/models/effects';
+import { Action } from '$lib/catalog/models/action';
+import { AttackEffect, DefendEffect } from '$lib/catalog/models/effects';
 import { Reaction } from '$lib/catalog/models/reaction';
 import { Trait } from '$lib/catalog/models/trait';
 import standard from '../../properties/standard';
@@ -11,6 +12,17 @@ export default new Trait({
 	},
 	properties: [standard],
 	capabilities: [
+		new Action({
+			cost: {
+				strength: 1
+			},
+			effects: [
+				new AttackEffect({
+					expression: 'strength',
+					damage: { '2': 1, '3': 2, '4': 3 }
+				})
+			]
+		}),
 		new Reaction({
 			triggers: ['receivingAttack'],
 			effects: [
