1b97906034a5f4ef537730997c28844243449917
marticongost <marticongost@gmail.com>
2026-01-16 17:37:21 +0100

    Add the BlockQuote component

---
+++ src/lib/components/BlockQuote.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { standardAttributes } from './standardattributes';
+</script>
+
+<blockquote {...standardAttributes($$props, 'block-quote')}>
+	<slot />
+</blockquote>
+
+<style lang="scss">
+	@use '@reguitzell/styles' as rz;
+
+	blockquote {
+		margin: 0;
+		margin-top: rz.size(sm);
+		@include rz.vpadding(xs);
+		padding-left: rz.size(md);
+		border-left: 4px solid var(--text-subtle-color);
+	}
+</style>
