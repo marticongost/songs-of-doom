ff0c8b6143ee37ec72b44b5c68013997f5df955c
marticongost <marticongost@gmail.com>
2026-01-16 18:27:23 +0100

    Improve the appearence of the BlockQuote component

--- src/lib/components/BlockQuote.svelte
+++ src/lib/components/BlockQuote.svelte
@@ -14,6 +14,7 @@
 		margin-top: rz.size(sm);
 		@include rz.vpadding(xs);
 		padding-left: rz.size(md);
-		border-left: 4px solid var(--text-subtle-color);
+		border-left: 4px solid rgba(white, 0.1);
+		background-color: rgba(white, 0.05);
 	}
 </style>
