4fea71db30a801da16204da68e34921017f434c1
marticongost <marticongost@gmail.com>
2026-01-16 23:52:12 +0100

    Use a square frame for aptitudes

--- src/lib/components/aptitudes/AptitudeIcon.svelte
+++ src/lib/components/aptitudes/AptitudeIcon.svelte
@@ -18,11 +18,18 @@
 
 	:global(.aptitude-icon) {
 		align-self: center;
+		border-radius: 0.2em;
+		padding: 0.05em;
+		width: 1.2em;
+		height: 1.2em;
+		filter: drop-shadow(0 0 0.8rem black);
 	}
 
 	@each $aptitude in strength, agility, intelligence, charisma, will, health, sanity, focus, any {
 		:global(.aptitude-icon[data-aptitude='#{$aptitude}']) {
+			background-color: white;
 			color: var(--aptitude-#{$aptitude}-color);
+			border: 1px solid var(--aptitude-#{$aptitude}-color);
 		}
 	}
 </style>
