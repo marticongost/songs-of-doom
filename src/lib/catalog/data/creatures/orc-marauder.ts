3864cd4eb5721bca3eb4e0b4420c04c14cbc2bbb
marticongost <marticongost@gmail.com>
2026-01-17 14:28:21 +0100

    Flesh out orc-marauder

--- src/lib/catalog/data/creatures/orc-marauder.ts
+++ src/lib/catalog/data/creatures/orc-marauder.ts
@@ -1,4 +1,12 @@
+import { WoundedCondition } from '$lib/catalog/models/conditions';
 import { Creature } from '$lib/catalog/models/creature';
+import {
+	AttackEffect,
+	ConditionalEffect,
+	DefendEffect,
+	ModifyRollEffect
+} from '$lib/catalog/models/effects';
+import { Reaction } from '$lib/catalog/models/reaction';
 
 export default new Creature({
 	title: {
@@ -13,5 +21,42 @@ export default new Creature({
 		charisma: 1,
 		will: 3,
 		health: 5
-	}
+	},
+	capabilities: [
+		new Reaction({
+			triggers: ['provoked'],
+			effects: [
+				new AttackEffect({
+					expression: 'strength',
+					damage: {
+						'1': 1,
+						'2': 3,
+						'3': 4,
+						'4': 5
+					}
+				})
+			]
+		}),
+		new Reaction({
+			triggers: ['attacking'],
+			effects: [
+				new ConditionalEffect({
+					cases: [
+						{
+							condition: new WoundedCondition(),
+							effects: [new ModifyRollEffect({ modifier: 1 })]
+						}
+					]
+				})
+			]
+		}),
+		new Reaction({
+			triggers: ['receivingAttack'],
+			effects: [
+				new DefendEffect({
+					expression: 'agility'
+				})
+			]
+		})
+	]
 });
