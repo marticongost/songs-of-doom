dd07676179a485dd5715afd98b44dd277c165c73
marticongost <marticongost@gmail.com>
2026-01-17 09:18:00 +0100

    Change the presentation of conditional effects, from a tree to a block quote

--- src/lib/components/effects/ConditionalEffectChip.svelte
+++ src/lib/components/effects/ConditionalEffectChip.svelte
@@ -1,33 +1,20 @@
 <script lang="ts">
 	import { ConditionalEffect, Effect, type Case } from '$lib/catalog/models/effects';
+	import BlockQuote from '../BlockQuote.svelte';
 	import ConditionChip from '../conditions/ConditionChip.svelte';
-	import InlineSvg from '../InlineSvg.svelte';
 	import Text from '../localisation/Text.svelte';
-	import Tree, { TreeContext } from '../Tree.svelte';
-	import EffectChip from './EffectChip.svelte';
+	import EffectList from './EffectList.svelte';
 	export let effect: ConditionalEffect;
 </script>
 
-{#snippet treeItem(context: TreeContext<Effect | Case>)}
-	{#if context.item instanceof Effect}
-		<EffectChip effect={context.item} />
-	{:else}
-		<Text ca="Si" es="Si" en="If" />
-		<ConditionChip condition={context.item.condition} />
-	{/if}
-{/snippet}
-
-<Tree
-	appearence="arrows"
-	root={effect as Effect | Case}
-	getChildren={(context) => {
-		if (context.item instanceof ConditionalEffect) {
-			return context.item.cases;
-		} else if (context.item instanceof Effect) {
-			return [];
-		} else {
-			return context.item.effects;
-		}
-	}}
-	itemSnippet={treeItem}
-/>
+{#each effect.cases as conditionCase}
+	<div class="case">
+		<div class="condition">
+			<Text ca="Si" es="Si" en="If" />
+			<ConditionChip condition={conditionCase.condition} />
+		</div>
+		<BlockQuote>
+			<EffectList effects={conditionCase.effects} />
+		</BlockQuote>
+	</div>
+{/each}
