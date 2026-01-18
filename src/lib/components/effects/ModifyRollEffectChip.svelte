1d92659d7480f8e3a5562729e8819f7507a8db6e
marticongost <marticongost@gmail.com>
2026-01-18 21:22:16 +0100

    Rephrase the text in ModifyRollEffectChip to match the new dice roll rules

--- src/lib/components/effects/ModifyRollEffectChip.svelte
+++ src/lib/components/effects/ModifyRollEffectChip.svelte
@@ -1,29 +1,17 @@
 <script lang="ts">
 	import { ModifyRollEffect } from '$lib/catalog/models/effects';
 	import Text from '$lib/components/localisation/Text.svelte';
-	import { plural2 } from '$lib/localisation';
 
 	interface Props {
 		effect: ModifyRollEffect;
 	}
 
 	const { effect }: Props = $props();
+	const modifierText = effect.modifier > 0 ? `+${effect.modifier}` : effect.modifier.toString();
 </script>
 
 <Text
-	ca="{effect.modifier > 0 ? 'Afegir' : 'Restar'} {Math.abs(effect.modifier)} {plural2(
-		Math.abs(effect.modifier),
-		'dau',
-		'daus'
-	)} a la tirada"
-	es="{effect.modifier > 0 ? 'Añadir' : 'Restar'} {Math.abs(effect.modifier)} {plural2(
-		Math.abs(effect.modifier),
-		'dado',
-		'dados'
-	)} a la tirada"
-	en="{effect.modifier > 0 ? 'Add' : 'Subtract'} {Math.abs(effect.modifier)} {plural2(
-		Math.abs(effect.modifier),
-		'die',
-		'dice'
-	)} to the roll"
+	ca="Modificar la tirada en {modifierText}"
+	es="Modificar la tirada en {modifierText}"
+	en="Modify the roll by {modifierText}"
 />
