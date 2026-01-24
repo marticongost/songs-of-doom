10340d41022548c85f332fea0e89d67e3e005eeb
marticongost <marticongost@gmail.com>
2026-01-24 13:37:18 +0100

    Overhaul CommaSeparatedList, support lists with no conjunction

--- src/lib/components/localisation/CommaSeparatedList.svelte
+++ src/lib/components/localisation/CommaSeparatedList.svelte
@@ -1,29 +1,49 @@
+<script lang="ts" module>
+	type ConjunctionType = 'none' | 'and' | 'or';
+	const orText = {
+		ca: ' o ',
+		es: ' o ',
+		en: ' or '
+	};
+	const andText = {
+		ca: ' i ',
+		es: ' y ',
+		en: ' and '
+	};
+
+	const getSeparator = (index: number, total: number, conjunction: ConjunctionType) => {
+		if (index === 0) {
+			return '';
+		}
+		if (index == total - 1) {
+			if (conjunction === 'and') {
+				return translate(andText, getLocale());
+			} else if (conjunction === 'or') {
+				return translate(orText, getLocale());
+			}
+		}
+		return ', ';
+	};
+</script>
+
 <script lang="ts" generics="T">
+	import { getLocale } from '$lib/context/locale';
+	import { translate } from '$lib/localisation';
 	import type { Snippet } from 'svelte';
 	import { standardAttributes } from '../standardattributes';
-	import Text from './Text.svelte';
 
 	interface Props {
 		items: T[];
 		renderItem: Snippet<[T]>;
-		operator?: 'and' | 'or';
+		conjunction?: ConjunctionType;
 	}
 
-	const { items, renderItem, operator = 'and', ...attributes }: Props = $props();
+	const { items, renderItem, conjunction = 'and', ...attributes }: Props = $props();
 </script>
 
 <span {...standardAttributes(attributes, 'comma-separated-list')}>
 	{#each items as item, index}
-		{@render renderItem(item)}
-		{#if index === items.length - 2}
-			{#if operator === 'or'}
-				<Text ca=" o " es=" o " en=" or " />
-			{:else if operator === 'and'}
-				<Text ca=" i " es=" y " en=" and " />
-			{/if}
-		{:else if index < items.length - 2}
-			{', '}
-		{/if}
+		{getSeparator(index, items.length, conjunction)}{@render renderItem(item)}
 	{/each}
 </span>
 
@@ -32,6 +52,7 @@
 
 	.comma-separated-list {
 		display: inline;
+		white-space: break-spaces;
 
 		& > * {
 			display: inline;
