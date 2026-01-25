8af351b3f2a9a63603ae99b1ee29e15a021a774f
marticongost <marticongost@gmail.com>
2026-01-25 21:10:09 +0100

    Add PropertyCondition

--- src/lib/components/conditions/ConditionChip.svelte
+++ src/lib/components/conditions/ConditionChip.svelte
@@ -3,12 +3,14 @@
 		DistanceCondition,
 		EngagedCondition,
 		NearbyEnemiesCondition,
+		PropertyCondition,
 		WoundedCondition,
 		type Condition
 	} from '$lib/catalog/models/conditions';
 	import DistanceConditionChip from './DistanceConditionChip.svelte';
 	import EngagedConditionChip from './EngagedConditionChip.svelte';
 	import NearbyEnemiesConditionChip from './NearbyEnemiesConditionChip.svelte';
+	import PropertyConditionChip from './PropertyConditionChip.svelte';
 	import WoundedConditionChip from './WoundedConditionChip.svelte';
 
 	interface Props {
@@ -24,6 +26,8 @@
 	<EngagedConditionChip {condition} />
 {:else if condition instanceof NearbyEnemiesCondition}
 	<NearbyEnemiesConditionChip {condition} />
+{:else if condition instanceof PropertyCondition}
+	<PropertyConditionChip {condition} />
 {:else if condition instanceof WoundedCondition}
 	<WoundedConditionChip {condition} />
 {/if}
---
+++ src/lib/components/conditions/PropertyConditionChip.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { PropertyCondition } from '$lib/catalog/models/conditions';
+	import PropertyList from '../properties/PropertyList.svelte';
+
+	interface Props {
+		condition: PropertyCondition;
+	}
+
+	const { condition }: Props = $props();
+</script>
+
+<PropertyList properties={condition.properties} />
