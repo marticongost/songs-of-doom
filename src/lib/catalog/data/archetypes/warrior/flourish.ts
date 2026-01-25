406765997696c7a2f2eb32fbaf25db1d767c618f
marticongost <marticongost@gmail.com>
2026-01-25 21:14:31 +0100

    Develop flourish

--- src/lib/catalog/data/archetypes/warrior/flourish.ts
+++ src/lib/catalog/data/archetypes/warrior/flourish.ts
@@ -1,4 +1,9 @@
+import { PropertyCondition } from '$lib/catalog/models/conditions';
+import { DrawAptitudeEffect, ResultsTableEffect } from '$lib/catalog/models/effects';
+import { RechargeEffect } from '$lib/catalog/models/effects/recharge';
+import { Opportunity } from '$lib/catalog/models/reaction';
 import { Skill } from '$lib/catalog/models/skill';
+import weapon from '../../properties/weapon';
 
 export default new Skill({
 	title: {
@@ -7,6 +12,30 @@ export default new Skill({
 		en: 'Flourish'
 	},
 	xpCost: 0,
-	discardReward: { agility: 1 }
-	// TODO: add charges to an asset
+	discardReward: { agility: 1 },
+	capabilities: [
+		new Opportunity({
+			triggers: ['attacking'],
+			cost: { agility: 2 },
+			effects: [
+				new ResultsTableEffect({
+					entries: [
+						{
+							result: '2+',
+							effects: [
+								new RechargeEffect({
+									amount: 1,
+									target: {
+										type: 'ownedObject',
+										conditions: [new PropertyCondition({ properties: [weapon] })]
+									}
+								}),
+								new DrawAptitudeEffect({ amount: 1 })
+							]
+						}
+					]
+				})
+			]
+		})
+	]
 });
