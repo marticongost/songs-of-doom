aede87e90bf3a3cbc6dbef05e8e2ad9125de23bc
marticongost <marticongost@gmail.com>
2026-01-25 20:28:25 +0100

    Develop exposed-attack

--- src/lib/catalog/data/archetypes/warrior/exposed-attack.ts
+++ src/lib/catalog/data/archetypes/warrior/exposed-attack.ts
@@ -1,3 +1,10 @@
+import { Action } from '$lib/catalog/models/action';
+import {
+	ModifyDamageEffect,
+	ResultsTableEffect,
+	TriggerAttackEffect
+} from '$lib/catalog/models/effects';
+import { ReceiveOpportunityAttacksEffect } from '$lib/catalog/models/effects/receiveopportunityattacks';
 import { Skill } from '$lib/catalog/models/skill';
 
 export default new Skill({
@@ -9,6 +16,29 @@ export default new Skill({
 	xpCost: 0,
 	discardReward: {
 		agility: 1
-	}
+	},
+	capabilities: [
+		new Action({
+			effects: [
+				new TriggerAttackEffect({
+					modifiers: [
+						new ResultsTableEffect({
+							entries: [
+								{
+									result: '1+',
+									effects: [new ModifyDamageEffect({ amount: 2 })]
+								},
+								{
+									result: 0,
+									effects: [new ReceiveOpportunityAttacksEffect({})]
+								}
+							]
+						})
+					]
+				})
+			]
+		})
+	]
+
 	// TODO: atac +2, si falla, l'oponent ataca amb +1 sense cost
 });
