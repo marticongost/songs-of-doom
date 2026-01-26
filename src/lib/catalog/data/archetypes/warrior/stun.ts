a07f5d48b3e1d765e697e310be3b878e184fdd2f
marticongost <marticongost@gmail.com>
2026-01-26 19:11:30 +0100

    Develop the stun card

--- src/lib/catalog/data/archetypes/warrior/stun.ts
+++ src/lib/catalog/data/archetypes/warrior/stun.ts
@@ -1,3 +1,5 @@
+import { NegateDamageEffect, RemoveChargesEffect } from '$lib/catalog/models/effects';
+import { Opportunity } from '$lib/catalog/models/reaction';
 import { Skill } from '$lib/catalog/models/skill';
 
 export default new Skill({
@@ -7,6 +9,18 @@ export default new Skill({
 		en: 'Stun'
 	},
 	xpCost: 0,
-	discardReward: { strength: 1 }
+	discardReward: { strength: 1 },
+	capabilities: [
+		new Opportunity({
+			triggers: ['attacking'],
+			cost: {
+				strength: 2
+			},
+			effects: [
+				new NegateDamageEffect(),
+				new RemoveChargesEffect({ target: 'defender', amount: 'result' })
+			]
+		})
+	]
 	// TODO: Opportunity, remove charges from enemy if attack was successful
 });
