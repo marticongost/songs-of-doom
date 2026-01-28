5fb225dc399123fb068d85294521010f6e9f6f2e
marticongost <marticongost@gmail.com>
2026-01-28 09:11:10 +0100

    Add the `stroke-of-luck` card

---
+++ src/lib/catalog/data/archetypes/adventurer/stroke-of-luck.ts
@@ -0,0 +1,23 @@
+import { RedrawFateEffect, ResultsTableEffect } from '$lib/catalog/models/effects';
+import { Opportunity } from '$lib/catalog/models/reaction';
+import { Skill } from '$lib/catalog/models/skill';
+
+export default new Skill({
+	title: {
+		ca: 'Cop de surt',
+		es: 'Golpe de suerte',
+		en: 'Stroke of luck'
+	},
+	discardReward: {
+		intelligence: 1
+	},
+	capabilities: [
+		new Opportunity({
+			triggers: ['fateDrawn'],
+			cost: { any: 1 },
+			effects: [
+				new ResultsTableEffect({ entries: [{ result: 0, effects: [new RedrawFateEffect()] }] })
+			]
+		})
+	]
+});
