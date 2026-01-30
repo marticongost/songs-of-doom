1d83c94b95fb1cb4b959d87229feda335a92e4df
marticongost <marticongost@gmail.com>
2026-01-30 20:41:00 +0100

    Add the projectile rule

---
+++ src/lib/catalog/data/properties/projectile.ts
@@ -0,0 +1,14 @@
+import { Rule } from '$lib/catalog/models/properties/rule';
+
+export default new Rule({
+	title: {
+		ca: 'Projectil',
+		es: 'Proyectil',
+		en: 'Projectile'
+	},
+	description: {
+		ca: "Aquest atac no necessita estar enfrontat amb l'objectiu.",
+		es: 'Este ataque no necesita estar enfrentado con el objetivo.',
+		en: 'This attack does not need to be facing the target.'
+	}
+});
