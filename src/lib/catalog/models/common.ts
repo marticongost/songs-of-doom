417d38f8f63429c8d19860b17b08984f1a35596e
marticongost <marticongost@gmail.com>
2026-01-31 10:07:18 +0100

    Add the shootBeforeEngaged definition snippet

--- src/lib/catalog/models/common.ts
+++ src/lib/catalog/models/common.ts
@@ -1,7 +1,17 @@
+import { TriggerAttackEffect } from './effects';
 import { RechargeEffect } from './effects/recharge';
-import { Obligation } from './reaction';
+import { Obligation, Opportunity } from './reaction';
 
 export const fullyRechargeOnChapterStart = new Obligation({
 	triggers: ['chapterStart'],
 	effects: [new RechargeEffect({ amount: 'max' })]
+});
+
+export const shootBeforeEngaged = new Opportunity({
+	triggers: ['beforeEnemyEngagesWithSelf'],
+	effects: [
+		new TriggerAttackEffect({
+			card: 'this'
+		})
+	]
 });
