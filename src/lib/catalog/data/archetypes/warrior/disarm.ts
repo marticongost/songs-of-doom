0b8b09fdb0276c3825a70bfa55ec83a5950efdb3
marticongost <marticongost@gmail.com>
2026-02-05 22:47:52 +0100

    Add the disarm card

---
+++ src/lib/catalog/data/archetypes/warrior/disarm.ts
@@ -0,0 +1,57 @@
+import { Action, Obligation } from '$lib/catalog/models/capabilities';
+import {
+	AttachEffect,
+	ModifyCapabilityCostEffect,
+	ModifyDamageEffect,
+	ModifyRollEffect,
+	NegateDamageEffect,
+	ResultsTableEffect,
+	TriggerAttackEffect
+} from '$lib/catalog/models/effects';
+import { Skill } from '$lib/catalog/models/skill';
+import { upgradable } from '$lib/catalog/models/upgrades';
+
+export default upgradable(Skill, 2, (variants) => ({
+	title: {
+		ca: 'Desarmar',
+		en: 'Disarm',
+		es: 'Desarmar'
+	},
+	xpCost: variants.values(0, 1),
+	discardReward: { agility: 2 },
+	capabilities: [
+		new Action({
+			cost: {
+				agility: variants.level
+			},
+			effects: [
+				new TriggerAttackEffect({
+					modifiers: [
+						new ModifyCapabilityCostEffect({ cost: { agility: -variants.level } }),
+						new ResultsTableEffect({
+							entries: [
+								{
+									result: variants.values('2+', '1+'),
+									effects: [
+										new NegateDamageEffect(),
+										new AttachEffect({
+											target: {
+												type: 'defender'
+											}
+										})
+									]
+								}
+							]
+						})
+					]
+				})
+			]
+		})
+	],
+	attachmentCapabilities: [
+		new Obligation({
+			triggers: ['attacking'],
+			effects: [new ModifyRollEffect({ modifier: -2 }), new ModifyDamageEffect({ amount: -1 })]
+		})
+	]
+}));
