f700b07f702932ede08e96cb25970406f0696b02
marticongost <marticongost@gmail.com>
2026-02-05 18:05:57 +0100

    Add the first-aid card

---
+++ src/lib/catalog/data/archetypes/warrior/first-aid.ts
@@ -0,0 +1,24 @@
+import { Action } from '$lib/catalog/models/action';
+import { HealEffect } from '$lib/catalog/models/effects';
+import { Skill } from '$lib/catalog/models/skill';
+import { upgradable } from '$lib/catalog/models/upgrades';
+
+export default upgradable(Skill, 2, (variants) => ({
+	title: {
+		ca: 'Primers auxilis',
+		es: 'Primeros auxilios',
+		en: 'First aid'
+	},
+	xpCost: variants.values(0, 1),
+	discardReward: {
+		any: 1
+	},
+	capabilities: [
+		new Action({
+			cost: {
+				intelligence: 2
+			},
+			effects: [new HealEffect({ amount: variants.values(2, 3) })]
+		})
+	]
+}));
