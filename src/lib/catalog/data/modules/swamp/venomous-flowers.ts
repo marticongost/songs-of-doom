d46dab726fdcb815bfdfd680da1c67a2574bd7ed
marticongost <marticongost@gmail.com>
2026-02-11 20:13:45 +0100

    Flesh out venomous-flowers

--- src/lib/catalog/data/modules/swamp/venomous-flowers.ts
+++ src/lib/catalog/data/modules/swamp/venomous-flowers.ts
@@ -1,9 +1,28 @@
+import { Obligation } from '$lib/catalog/models/capabilities';
+import { DiscardFromHandEffect, TestEffect } from '$lib/catalog/models/effects';
 import { Encounter } from '$lib/catalog/models/encounter';
+import { handSize } from '$lib/catalog/models/expressions';
+import { minus } from '$lib/catalog/models/expressions/scalar-operation';
+import { intelligence } from '$lib/catalog/models/stats';
 
 export default new Encounter({
 	title: {
-		ca: 'Flors verinoses',
-		es: 'Flores venenosas',
+		ca: 'Flors emmetzinades',
+		es: 'Flores ponzoñosas',
 		en: 'Venomous Flowers'
-	}
+	},
+	capabilities: [
+		new Obligation({
+			triggers: ['revealed'],
+			effects: [
+				new TestEffect({
+					expression: minus(intelligence, handSize),
+					results: {
+						CF: [new DiscardFromHandEffect({ amount: 2, selection: 'random' })],
+						0: [new DiscardFromHandEffect({ amount: 1, selection: 'random' })]
+					}
+				})
+			]
+		})
+	]
 });
