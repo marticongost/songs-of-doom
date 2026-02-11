bcb64b43537747d567bda61b0383470c593ae350
marticongost <marticongost@gmail.com>
2026-02-11 18:18:46 +0100

    Improve ConditionalEffect
    - Support multiple effects in `default`
    - Render `default` in ConditionalEffectChip
    - Clarify rendering in ConditionalEffectChip
    - Add the ConditionalEffect.orElse helper method

--- src/lib/catalog/models/effects/conditional.ts
+++ src/lib/catalog/models/effects/conditional.ts
@@ -3,7 +3,7 @@ import { Effect } from './effect';
 
 export interface ConditionalEffectProps {
 	cases: Case[];
-	default?: Effect;
+	default?: Effect[];
 }
 
 export interface Case {
@@ -13,11 +13,18 @@ export interface Case {
 
 export class ConditionalEffect extends Effect {
 	readonly cases: Case[];
-	readonly default?: Effect;
+	readonly default?: Effect[];
 
 	constructor({ cases, default: defaultEffect }: ConditionalEffectProps) {
 		super();
 		this.cases = cases;
 		this.default = defaultEffect;
+	}
+
+	orElse(...effects: Effect[]): ConditionalEffect {
+		return new ConditionalEffect({
+			cases: this.cases,
+			default: [...(this.default ?? []), ...effects]
+		});
 	}
 }
--- src/lib/components/effects/ConditionalEffectChip.svelte
+++ src/lib/components/effects/ConditionalEffectChip.svelte
@@ -2,6 +2,7 @@
 	import { ConditionalEffect } from '$lib/catalog/models/effects';
 	import ExpressionChip from '$lib/components/expressions/ExpressionChip.svelte';
 	import ArrowIcon from '../ArrowIcon.svelte';
+	import Text from '../localisation/Text.svelte';
 	import EffectList from './EffectList.svelte';
 
 	interface Props {
@@ -12,9 +13,28 @@
 </script>
 
 {#each effect.cases as conditionCase, i (i)}
+	<span class="if">
+		<Text ca="SI" es="SI" en="IF" />
+	</span>
 	<span class="case">
 		<ExpressionChip expression={conditionCase.condition} />
 		<ArrowIcon />
 		<EffectList effects={conditionCase.effects} />
 	</span>
 {/each}
+{#if effect.default?.length}
+	<span class="else">
+		<Text ca="EN CAS CONTRARI" es="EN CASO CONTRARIO" en="ELSE" />
+	</span>
+	<ArrowIcon />
+	<EffectList effects={effect.default} />
+{/if}
+
+<style lang="scss">
+	@use '@reguitzell/styles' as rz;
+
+	.if,
+	.else {
+		font-weight: bold;
+	}
+</style>
