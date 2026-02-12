05755c7447e695e799120d4439fcb6b7c88b0ec6
marticongost <marticongost@gmail.com>
2026-02-12 18:20:25 +0100

    Flesh out toxic-fumes

--- src/lib/catalog/data/modules/swamp/toxic-fumes.ts
+++ src/lib/catalog/data/modules/swamp/toxic-fumes.ts
@@ -1,9 +1,25 @@
+import { Obligation } from '$lib/catalog/models/capabilities';
+import { GoTowardsEffect, TestEffect } from '$lib/catalog/models/effects';
 import { Encounter } from '$lib/catalog/models/encounter';
+import { strength } from '$lib/catalog/models/stats';
 
 export default new Encounter({
 	title: {
 		ca: 'Fums tòxics',
 		es: 'Humos tóxicos',
 		en: 'Toxic Fumes'
-	}
+	},
+	capabilities: [
+		new Obligation({
+			triggers: ['revealed'],
+			effects: [
+				new TestEffect({
+					expression: strength,
+					results: {
+						failed: [new GoTowardsEffect({ destination: { type: 'enemy', selection: 'closest' } })]
+					}
+				})
+			]
+		})
+	]
 });
