d3ef41f7ccd78a69831af2b53e6ea83c3a06eb4d
marticongost <marticongost@gmail.com>
2026-02-14 09:39:02 +0100

    Make CardButton open its card in a popover

--- src/lib/components/CardButton.svelte
+++ src/lib/components/CardButton.svelte
@@ -2,9 +2,12 @@
 	import type { Entity } from '$lib/catalog/models/entity';
 	import { resolve } from '$app/paths';
 	import { getLocale } from '$lib/context/locale';
+	import Button from './Button.svelte';
+	import Card from './Card.svelte';
 	import CardLevel from './CardLevel.svelte';
 	import ExperienceIndicator from './indicators/ExperienceIndicator.svelte';
 	import Text from './localisation/Text.svelte';
+	import Popover from './Popover.svelte';
 	import { standardAttributes, type StandardAttributeProps } from './standardattributes';
 
 	interface Props extends StandardAttributeProps {
@@ -12,20 +15,41 @@
 	}
 
 	const { entity, ...attributes }: Props = $props();
+
+	const popoverId = $derived(`card-popover-${entity.variantId}`);
+	const anchorName = $derived(`--${popoverId}`);
+	const cardHref = $derived(
+		resolve('/[locale]/cards/[id]', { locale: getLocale(), id: entity.variantId })
+	);
 </script>
 
-<a
-	{...standardAttributes(attributes, 'card-button')}
-	data-type={entity.type.id}
-	href={resolve('/[locale]/cards/[id]', { locale: getLocale(), id: entity.variantId })}
->
-	<div class="title"><Text {...entity.title} /></div>
-	<CardLevel {entity} />
-	<ExperienceIndicator amount={entity.xpCost ?? 0} />
-</a>
+<div {...standardAttributes(attributes, 'card-button-wrapper')}>
+	<button
+		class="card-button"
+		data-type={entity.type.id}
+		popovertarget={popoverId}
+		style:anchor-name={anchorName}
+	>
+		<div class="title"><Text {...entity.title} /></div>
+		<CardLevel {entity} />
+		<ExperienceIndicator amount={entity.xpCost ?? 0} />
+	</button>
+	<Popover id={popoverId} anchor={anchorName} position="right">
+		<div class="popover-content">
+			<Card {entity} linked={false} />
+			<Button href={cardHref}>
+				<Text ca="Obrir" es="Abrir" en="Open" />
+			</Button>
+		</div>
+	</Popover>
+</div>
 
 <style lang="scss">
 	@use '@reguitzell/styles' as rz;
+
+	.card-button-wrapper {
+		display: contents;
+	}
 
 	.card-button {
 		@include rz.row(sm);
@@ -33,6 +57,11 @@
 		@include rz.hpadding(md);
 		border: var(--panel-border);
 		border-radius: rz.size(sm);
+		cursor: pointer;
+		font-family: inherit;
+		font-size: inherit;
+		text-align: left;
+		width: 100%;
 
 		@each $type in archetype, trait, skill, ally, item, creature {
 			&[data-type='#{$type}'] {
@@ -51,5 +80,10 @@
 		color: var(--text-heading-color);
 		text-shadow: 0 0 0.5em rgba(0, 0, 0, 0.8);
 		margin-right: auto;
+	}
+
+	.popover-content {
+		@include rz.column(md);
+		align-items: center;
 	}
 </style>
