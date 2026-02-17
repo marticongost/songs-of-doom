f300bc952a5acd630d99dedbb83a6597dec2b757
marticongost <marticongost@gmail.com>
2026-02-17 20:47:30 +0100

    Exclude wrong card types in the character detail view

--- packages/web/src/routes/[locale]/characters/[id]/+page.svelte
+++ packages/web/src/routes/[locale]/characters/[id]/+page.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
 	import EntityCatalog from '$lib/components/EntityCatalog.svelte';
 	import { EntitySearchState } from '$lib/search';
-	import { entities, Module } from '@songsofdoom/game';
+	import { entities, type EntityTypeId } from '@songsofdoom/game';
 	import type { PageData } from './$types';
 
 	let { data: _data }: { data: PageData } = $props();
 
 	// All entities except modules
-	const allEntities = entities.all().filter((e) => !(e instanceof Module));
+	const allowedTypes: EntityTypeId[] = ['archetype', 'trait', 'skill', 'item', 'ally'];
+	const allEntities = entities.all().filter((e) => allowedTypes.includes(e.type.id));
 
 	const searchState = new EntitySearchState({
-		allowedTypes: ['archetype', 'trait', 'skill', 'item', 'ally'],
+		allowedTypes,
 		syncUrl: false
 	});
 </script>
