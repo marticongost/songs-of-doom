2676f0683eefa7c78b1c7d8ec08c48c14350f65b
marticongost <marticongost@gmail.com>
2026-02-22 13:12:02 +0100

    Add the mapToRecord function

--- packages/common/src/utils.ts
+++ packages/common/src/utils.ts
@@ -17,3 +17,42 @@ export const finalise = <T, Props, P extends T | Props | undefined>(
 
 	return new model(props as Props) as Finalised<P, T>;
 };
+
+export type MapToRecordOptions<
+	InputKey,
+	InputValue,
+	OutputKey extends string | number | symbol,
+	OutputValue
+> = {
+	mapKeys?: (key: InputKey) => OutputKey;
+	mapValues?: (value: InputValue) => OutputValue;
+	mapEntries?: (entry: [InputKey, InputValue]) => [OutputKey, OutputValue];
+};
+
+/** Converts a `Map` or `Record` into another `Record`, optionally mapping its keys
+ * and/or values in the process.
+ */
+export const mapToRecord = <
+	InputKey,
+	InputValue,
+	OutputKey extends string | number | symbol,
+	OutputValue
+>(
+	source: Map<InputKey, InputValue> | Record<string | number | symbol, InputValue>,
+	options: MapToRecordOptions<InputKey, InputValue, OutputKey, OutputValue>
+): Record<OutputKey, OutputValue> => {
+	const record = {} as Record<OutputKey, OutputValue>;
+	const entries =
+		source instanceof Map ? source.entries() : (Object.entries(source) as [InputKey, InputValue][]);
+	for (const [key, value] of entries) {
+		if (options.mapEntries) {
+			const [outputKey, outputValue] = options.mapEntries([key, value]);
+			record[outputKey] = outputValue;
+		} else {
+			const k = options.mapKeys ? options.mapKeys(key) : (key as unknown as OutputKey);
+			const v = options.mapValues ? options.mapValues(value) : (value as unknown as OutputValue);
+			record[k] = v;
+		}
+	}
+	return record;
+};
