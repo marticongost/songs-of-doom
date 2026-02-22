f3cf81b135ccedb2cb50eae0da1d5655b4e7ed40
marticongost <marticongost@gmail.com>
2026-02-22 18:31:40 +0100

    Add URL functions for characters

--- packages/web/src/lib/urls.ts
+++ packages/web/src/lib/urls.ts
@@ -1,6 +1,7 @@
 import type { Locale } from '@songsofdoom/common';
 import type { Entity } from '@songsofdoom/game';
 import { getLocale } from './context/locale';
+import type { Character } from './models/characters';
 
 /** Gets the URL for a given entity.
  *
@@ -8,6 +9,25 @@ import { getLocale } from './context/locale';
  * @param locale An optional locale to include in the URL. If not provided, the current locale will be used.
  * @returns The URL for the given entity in the chosen locale.
  */
-export const getEntityUrl = (entity: Entity, locale?: Locale): string => {
-	return `/${locale ?? getLocale()}/cards/${entity.variantId}`;
+export const getEntityUrl = (entity: Entity | string, locale?: Locale): string => {
+	return `/${locale ?? getLocale()}/cards/${typeof entity === 'string' ? entity : entity.variantId}`;
+};
+
+/** Gets the URL to create a new character.
+ *
+ * @param locale An optional locale to include in the URL. If not provided, the current locale will be used.
+ * @returns The URL for creating a new character, in the chosen locale.
+ */
+export const getNewCharacterUrl = (locale?: Locale): string => {
+	return `/${locale ?? getLocale()}/characters/new`;
+};
+
+/** Gets the URL for a given character.
+ *
+ * @param character The character to get the URL for.
+ * @param locale An optional locale to include in the URL. If not provided, the current locale will be used.
+ * @returns The URL for the given character in the chosen locale.
+ */
+export const getCharacterUrl = (character: Character | number, locale?: Locale): string => {
+	return `/${locale ?? getLocale()}/characters/${typeof character === 'number' ? character : character.id}`;
 };
