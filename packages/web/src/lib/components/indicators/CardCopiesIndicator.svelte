3900395db1980761f4c52c05eaec5e49c81c54a9
marticongost <marticongost@gmail.com>
2026-02-23 20:14:27 +0100

    Add the CardCopiesIndicator component

---
+++ packages/web/src/lib/components/indicators/CardCopiesIndicator.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { standardAttributes, type StandardAttributeProps } from '../standardattributes';
+	import Indicator from './Indicator.svelte';
+
+	interface Props extends StandardAttributeProps {
+		amount: number;
+	}
+
+	const { amount, ...attributes }: Props = $props();
+</script>
+
+<Indicator {...standardAttributes(attributes, 'card-copies-indicator')} {amount} icon="cards.svg" />
+
+<style lang="scss">
+	:global(.card-copies-indicator) {
+		--indicator-value-text-shadow: 0 0 0.2em black;
+		--indicator-color: var(--card-copies-indicator-color);
+	}
+</style>
