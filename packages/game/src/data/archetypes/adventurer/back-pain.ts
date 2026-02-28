1a91cacf9355346516389807b89b241437d6028c
marticongost <marticongost@gmail.com>
2026-02-28 17:55:56 +0100

    Add the "back-pain" card

---
+++ packages/game/src/data/archetypes/adventurer/back-pain.ts
@@ -0,0 +1,15 @@
+import { Constant } from '../../../models/capabilities';
+import { ModifyCarryingCapacityEffect } from '../../../models/effects';
+import { Trait } from '../../../models/trait';
+import { flaw } from '../../properties';
+
+export default new Trait({
+	title: { ca: "Dolor d'esquena", es: 'Dolor de espalda', en: 'Back Pain' },
+	xpCost: -2,
+	properties: [flaw],
+	capabilities: [
+		new Constant({
+			effects: [new ModifyCarryingCapacityEffect({ modifier: -1 })]
+		})
+	]
+});
