2076dbc54a28b068e981d3230cb4b35187660c16
marticongost <marticongost@gmail.com>
2026-03-01 18:01:14 +0100

    Add the "fast-attack" card

---
+++ packages/game/src/data/archetypes/rogue/fast-attack.ts
@@ -0,0 +1,24 @@
+import { Action } from '../../../models/capabilities';
+import { ModifyRollEffect, TriggerAttackEffect } from '../../../models/effects';
+import { Skill } from '../../../models/skill';
+import { upgradable } from '../../../models/upgrades';
+
+export default upgradable(Skill, 2, (variants) => ({
+	title: {
+		ca: 'Atac precipitat',
+		es: 'Ataque apresurado',
+		en: 'Fast attack'
+	},
+	xpCost: variants.values(0, 2),
+	discardReward: { agility: variants.level },
+	capabilities: [
+		new Action({
+			fast: true,
+			effects: [
+				new TriggerAttackEffect({
+					modifiers: [new ModifyRollEffect({ modifier: -1 })]
+				})
+			]
+		})
+	]
+}));
