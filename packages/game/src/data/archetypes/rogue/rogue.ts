81f6e800ffa204e7339cf439064bfd4b5a39bf88
marticongost <marticongost@gmail.com>
2026-03-01 14:47:28 +0100

    Add the "rogue" archetype

---
+++ packages/game/src/data/archetypes/rogue/rogue.ts
@@ -0,0 +1,23 @@
+import { Archetype } from '../../../models/archetype';
+import { Opportunity } from '../../../models/capabilities';
+import { TransformFocusEffect } from '../../../models/effects';
+
+export default new Archetype({
+	title: {
+		ca: 'Lladre',
+		es: 'Ladrón',
+		en: 'Rogue'
+	},
+	xpCost: 5,
+	capabilities: [
+		new Opportunity({
+			triggers: ['payingCapability'],
+			cost: { cardTransition: 'exhaust' },
+			effects: [
+				new TransformFocusEffect({
+					target: 'agility'
+				})
+			]
+		})
+	]
+});
