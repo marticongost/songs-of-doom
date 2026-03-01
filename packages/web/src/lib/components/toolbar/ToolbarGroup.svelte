a202046328587d3ec98122a539e4ff2d1157508f
marticongost <marticongost@gmail.com>
2026-03-01 14:46:40 +0100

    Add the ToolbarGroup component

---
+++ packages/web/src/lib/components/toolbar/ToolbarGroup.svelte
@@ -0,0 +1,37 @@
+<!--
+@component
+Groups related toolbar elements together and renders a thin vertical separator after the
+group if it is followed by any other element in the toolbar.
+-->
+<script lang="ts">
+	import {
+		standardAttributes,
+		type StandardAttributeProps
+	} from '$lib/components/standardattributes';
+	import type { Snippet } from 'svelte';
+
+	interface Props extends StandardAttributeProps {
+		/** The toolbar elements to group together. */
+		children: Snippet;
+	}
+
+	const { children, ...rest }: Props = $props();
+</script>
+
+<div {...standardAttributes(rest, 'toolbar-group')}>
+	{@render children()}
+</div>
+
+<style lang="scss">
+	@use '@reguitzell/styles' as rz;
+
+	.toolbar-group {
+		@include rz.row;
+		border-left: 1px dotted var(--toolbar-separator-color);
+		border-right: 1px dotted var(--toolbar-separator-color);
+	}
+
+	.toolbar-group :global(+ .toolbar-group) {
+		border-left: none;
+	}
+</style>
