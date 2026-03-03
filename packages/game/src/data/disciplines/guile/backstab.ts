51d3a479f538d936150496033f209f9ecdac271c
marticongost <marticongost@gmail.com>
2026-03-03 20:50:02 +0100

    Flesh out the 'backstab' card

--- packages/game/src/data/disciplines/guile/backstab.ts
+++ packages/game/src/data/disciplines/guile/backstab.ts
@@ -1,5 +1,14 @@
+import { Action } from '../../../models/capabilities';
+import {
+	ConferPropertiesEffect,
+	ModifyCapabilityCostEffect,
+	ModifyRollEffect,
+	TriggerAttackEffect
+} from '../../../models/effects';
+import { engaged, not } from '../../../models/expressions';
 import { Skill } from '../../../models/skill';
 import { upgradable } from '../../../models/upgrades';
+import { piercing, projectile } from '../../properties';
 
 export default upgradable(Skill, 2, (variants) => ({
 	title: {
@@ -8,6 +17,24 @@ export default upgradable(Skill, 2, (variants) => ({
 		en: 'Backstab'
 	},
 	xpCost: variants.values(0, 2),
-	discardReward: { agility: variants.level },
-	capabilities: []
+	discardReward: { agility: 1, intelligence: variants.level },
+	capabilities: [
+		new Action({
+			cost: { intelligence: 1, agility: 1 },
+			effects: [
+				not(engaged).then(
+					new TriggerAttackEffect({
+						card: { condition: not(projectile) },
+						modifiers: [
+							new ModifyCapabilityCostEffect({ cost: { any: -2 } }),
+							new ModifyRollEffect({ modifier: variants.level }),
+							new ConferPropertiesEffect({
+								properties: [piercing.with({ value: variants.level + 1 })]
+							})
+						]
+					})
+				)
+			]
+		})
+	]
 }));
