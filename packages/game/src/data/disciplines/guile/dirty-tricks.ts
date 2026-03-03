206903753e0615b05d84a52c93d6890f96d1f69d
marticongost <marticongost@gmail.com>
2026-03-03 22:59:39 +0100

    Flesh out dirty-trick

--- packages/game/src/data/disciplines/guile/dirty-tricks.ts
+++ packages/game/src/data/disciplines/guile/dirty-tricks.ts
@@ -1,3 +1,11 @@
+import { Action } from '../../../models/capabilities';
+import {
+	AddChargesEffect,
+	AttachEffect,
+	discard,
+	RemoveChargesEffect
+} from '../../../models/effects';
+import { charges, distance, eq } from '../../../models/expressions';
 import { Skill } from '../../../models/skill';
 import { upgradable } from '../../../models/upgrades';
 
@@ -8,6 +16,21 @@ export default upgradable(Skill, 2, (variants) => ({
 		en: 'Dirty tricks'
 	},
 	xpCost: variants.values(0, 2),
-	discardReward: { agility: variants.level },
-	capabilities: []
+	discardReward: { intelligence: variants.level },
+	capabilities: [
+		new Action({
+			cost: { intelligence: 2 },
+			effects: [
+				new AttachEffect({ target: { type: 'enemy', condition: eq(distance, 0) } }),
+				new AddChargesEffect({ amount: variants.level + 1 })
+			]
+		})
+	],
+	maxCharges: variants.level + 1,
+	attachmentCapabilities: [
+		new Action({
+			prioritary: true,
+			effects: [new RemoveChargesEffect({ amount: 1 }), eq(charges, 0).then(discard)]
+		})
+	]
 }));
