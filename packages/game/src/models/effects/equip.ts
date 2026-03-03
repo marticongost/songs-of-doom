efca01011a2c5e963cf068796cf7e8d7bd013c42
marticongost <marticongost@gmail.com>
2026-03-03 17:37:19 +0100

    Make EquipEffect take a target parameter

--- packages/game/src/models/effects/equip.ts
+++ packages/game/src/models/effects/equip.ts
@@ -1,12 +1,27 @@
+import { finalise } from '@songsofdoom/common';
+import { Target, type ObjectTargetType, type TargetSpec } from '../target';
 import { Effect } from './effect';
+
+/** Constructor parameters for the {@link EquipEffect} class. */
+export interface EquipEffectProps {
+	/** The object(s) to equip. */
+	target: TargetSpec<ObjectTargetType>;
+}
 
 /**
  * An effect that allows the player to change their active equipment.
  * This lets the player swap their currently equipped items during play.
  */
-export class EquipEffect extends Effect {}
+export class EquipEffect extends Effect {
+	readonly target: Target<ObjectTargetType>;
+
+	constructor({ target }: EquipEffectProps) {
+		super();
+		this.target = finalise(Target, target);
+	}
+}
 
 /**
  * Singleton instance for changing active equipment.
  */
-export const equip = new EquipEffect();
+export const equip = new EquipEffect({ target: { type: 'object', cardinality: 'multiple' } });
--- packages/web/src/lib/components/effects/EquipEffectChip.svelte
+++ packages/web/src/lib/components/effects/EquipEffectChip.svelte
@@ -3,18 +3,16 @@
 	Displays an EquipEffect, which allows the player to change their active equipment.
 -->
 <script lang="ts">
-	import { type EquipEffect } from '@songsofdoom/game';
 	import Text from '$lib/components/localisation/Text.svelte';
+	import { type EquipEffect } from '@songsofdoom/game';
+	import TargetChip from '../targets/TargetChip.svelte';
 
 	interface Props {
 		effect: EquipEffect;
 	}
 
-	const { effect: _effect }: Props = $props();
+	const { effect }: Props = $props();
 </script>
 
-<Text
-	ca="Canviar l'equipament actiu"
-	es="Cambiar el equipamiento activo"
-	en="Change active equipment"
-/>
+<Text ca="Equipar" es="Equipar" en="Equip" />
+<TargetChip target={effect.target} />
