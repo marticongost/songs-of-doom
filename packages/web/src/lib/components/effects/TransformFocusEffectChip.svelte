aeec3001c4ff36b0cd798352a6fdc10b99acd122
marticongost <marticongost@gmail.com>
2026-03-04 20:09:59 +0100

    Phrasing fix in TransformFocusEffectChip

--- packages/web/src/lib/components/effects/TransformFocusEffectChip.svelte
+++ packages/web/src/lib/components/effects/TransformFocusEffectChip.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { TransformFocusEffect } from '@songsofdoom/game';
 	import Text from '$lib/components/localisation/Text.svelte';
+	import { TransformFocusEffect } from '@songsofdoom/game';
 	import FocusIcon from '../focuses/FocusIcon.svelte';
 
 	interface Props {
@@ -11,8 +11,8 @@
 </script>
 
 <Text
-	ca="Transformar una %(source) en %(target)"
-	es="Transformar una %(source) en %(target)"
+	ca="Transformar un %(source) en %(target)"
+	es="Transformar un %(source) en %(target)"
 	en="Transform one %(source) into %(target)"
 >
 	{#snippet source()}<FocusIcon focus={effect.source} />{/snippet}
