3856f5fd2cd6a296cdd88b824a3010e88b1ff8be
marticongost <marticongost@gmail.com>
2026-03-05 19:44:22 +0100

    Fix stroke-of-luck
    Make it work with critical failures

--- packages/game/src/data/archetypes/adventurer/stroke-of-luck.ts
+++ packages/game/src/data/archetypes/adventurer/stroke-of-luck.ts
@@ -18,7 +18,7 @@ export default upgradable(Skill, 2, (variants) => ({
 			triggers: ['fateDrawn'],
 			cost: { any: 1 },
 			effects: variants.values(
-				[new ResultsTableEffect({ entries: [{ result: 0, effects: [redrawFate] }] })],
+				[new ResultsTableEffect({ entries: [{ result: ['CF', 0], effects: [redrawFate] }] })],
 				[...variants.ifMatches(2, redrawFate)]
 			)
 		})
