d789a56b6df425ab19396141294910ae3c6d2149
marticongost <marticongost@gmail.com>
2026-03-12 18:45:14 +0100

    Exclude Campaign from the card listings

--- packages/web/src/routes/[locale]/cards/+page.ts
+++ packages/web/src/routes/[locale]/cards/+page.ts
@@ -1,7 +1,6 @@
-import { entities } from '@songsofdoom/game';
-import type { Entity } from '@songsofdoom/game';
-import { Discipline, Module } from '@songsofdoom/game';
 import { translate, type Locale } from '@songsofdoom/common/localisation';
+import type { Entity, EntityTypeId } from '@songsofdoom/game';
+import { entities } from '@songsofdoom/game';
 
 interface CardsPageData {
 	title: string;
@@ -9,12 +8,12 @@ interface CardsPageData {
 	locale: Locale;
 }
 
+const excludedCardTypes: Set<EntityTypeId> = new Set(['module', 'discipline', 'campaign']);
+
 export const load = ({ params }: { params: { locale: Locale } }): CardsPageData => {
 	return {
 		title: translate({ ca: 'Cartes', es: 'Cartas', en: 'Cards' }, params.locale),
-		entities: entities
-			.all()
-			.filter((entity) => !(entity instanceof Module) && !(entity instanceof Discipline)),
+		entities: entities.all().filter((entity) => !excludedCardTypes.has(entity.type.id)),
 		locale: params.locale
 	};
 };
