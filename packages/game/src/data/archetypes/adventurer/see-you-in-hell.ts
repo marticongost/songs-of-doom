f6f3c339eae0bccfcccdca357f0cdb49aa01afe7
marticongost <marticongost@gmail.com>
2026-03-15 19:59:56 +0100

    Add the `see-you-in-hell` card

---
+++ packages/game/src/data/archetypes/adventurer/see-you-in-hell.ts
@@ -0,0 +1,27 @@
+import { Opportunity } from '../../../models/capabilities';
+import { ModifyRollEffect } from '../../../models/effects';
+import { Skill } from '../../../models/entities/skill';
+import { lte } from '../../../models/expressions';
+import { remainingWounds } from '../../../models/expressions/scalar/wounds';
+import { upgradable } from '../../../models/upgrades';
+
+export default upgradable(Skill, 2, (variants) => ({
+	title: {
+		ca: "Ens veurem a l'infern",
+		es: 'Nos vemos en el infierno',
+		en: 'See you in hell'
+	},
+	discardReward: {
+		will: variants.level
+	},
+	xpCost: variants.values(0, 1),
+	capabilities: [
+		new Opportunity({
+			triggers: ['resolvingEncounter'],
+			cost: { will: 2 },
+			effects: [
+				lte(remainingWounds, 3).then(new ModifyRollEffect({ modifier: variants.values(2, 3) }))
+			]
+		})
+	]
+}));
