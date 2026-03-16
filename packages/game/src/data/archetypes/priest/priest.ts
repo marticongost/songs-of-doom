f8dad08e06681fb6fad017d391989f702f332354
marticongost <marticongost@gmail.com>
2026-03-16 20:12:31 +0100

    Add a placeholder for the 'priest' archetype

---
+++ packages/game/src/data/archetypes/priest/priest.ts
@@ -0,0 +1,28 @@
+import { Constant, Opportunity } from '../../../models/capabilities';
+import { TalentEffect, TransformFocusEffect } from '../../../models/effects';
+import { Archetype } from '../../../models/entities';
+import { lightArmour, mediumArmour } from '../../talents';
+
+export default new Archetype({
+	title: {
+		ca: 'Sacerdot',
+		es: 'Sacerdote',
+		en: 'Priest'
+	},
+	disciplines: ['might', 'endurance'],
+	xpCost: 5,
+	capabilities: [
+		new Constant({
+			effects: [new TalentEffect({ talents: [lightArmour, mediumArmour] })]
+		}),
+		new Opportunity({
+			triggers: ['payingCapability'],
+			cost: { cardTransition: 'exhaust' },
+			effects: [
+				new TransformFocusEffect({
+					target: 'will'
+				})
+			]
+		})
+	]
+});
