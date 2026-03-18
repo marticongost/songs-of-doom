3ad3eb7e8b02271adc3c13a9dce20e5a774e4d6e
marticongost <marticongost@gmail.com>
2026-03-18 18:26:57 +0100

    Add the 'retribution' card

---
+++ packages/game/src/data/disciplines/judgement/retribution.ts
@@ -0,0 +1,50 @@
+import { Action, Opportunity } from '../../../models/capabilities';
+import {
+	addCharges,
+	attach,
+	discard,
+	modifyDamage,
+	modifyRoll,
+	saveTargetToVariable,
+	triggerAttack
+} from '../../../models/effects';
+import { Skill } from '../../../models/entities';
+import { upgradable } from '../../../models/upgrades';
+
+export default upgradable(Skill, 2, (variants) => ({
+	title: {
+		ca: 'Retribució',
+		es: 'Retribución',
+		en: 'Retribution'
+	},
+	xpCost: variants.values(0, 1),
+	discardReward: {
+		strength: variants.level
+	},
+	capabilities: [
+		new Opportunity({
+			triggers: ['takingDamage'],
+			cost: { strength: 2 },
+			effects: [
+				saveTargetToVariable({ name: 'X', value: { type: 'attacker' } }),
+				attach(),
+				addCharges(variants.values(2, 3))
+			]
+		})
+	],
+	attachmentCapabilities: [
+		new Action({
+			cost: { charges: 1 },
+			effects: [
+				triggerAttack({
+					target: { variable: 'X' },
+					modifiers: [modifyRoll(2), modifyDamage(1)]
+				})
+			]
+		}),
+		new Opportunity({
+			triggers: ['fullyDischarged'],
+			effects: [discard()]
+		})
+	]
+}));
