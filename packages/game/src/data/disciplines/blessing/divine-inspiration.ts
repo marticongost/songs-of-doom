c56a8a2be08ffa949a6af65bc9fd149f16394963
marticongost <marticongost@gmail.com>
2026-03-18 17:03:59 +0100

    Add the divine-inspiration card

---
+++ packages/game/src/data/disciplines/blessing/divine-inspiration.ts
@@ -0,0 +1,23 @@
+import { Action } from '../../../models/capabilities';
+import { gatherClues } from '../../../models/effects';
+import { Skill } from '../../../models/entities';
+import { upgradable } from '../../../models/upgrades';
+
+export default upgradable(Skill, 2, (variants) => ({
+	title: {
+		ca: 'Inspiració divina',
+		es: 'Inspiración divina',
+		en: 'Divine Inspiration'
+	},
+	xpCost: variants.values(0, 1),
+	maxCharges: variants.values(2, 3),
+	discardReward: {
+		will: variants.level
+	},
+	capabilities: [
+		new Action({
+			cost: { will: 2 },
+			effects: [gatherClues({ amount: variants.values(1, 2) })]
+		})
+	]
+}));
