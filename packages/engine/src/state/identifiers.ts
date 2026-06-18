9b09a0eba5004492f66bc9a2d362f25b27bf961e
marticongost <marticongost@gmail.com>
2026-06-18 12:33:52 +0200

    Fix isAllyId

--- packages/engine/src/state/identifiers.ts
+++ packages/engine/src/state/identifiers.ts
@@ -31,7 +31,7 @@ export const isCardId = (id: EntityId): id is CardId => !id.startsWith('plr');
 export const isPlayerId = (id: EntityId): id is PlayerId => id.startsWith('plr');
 export const isLocationId = (id: EntityId): id is LocationId => id.startsWith('loc');
 export const isCreatureId = (id: EntityId): id is CreatureId => id.startsWith('crt');
-export const isAllyId = (id: EntityId): id is AllyId => id.startsWith('crt');
+export const isAllyId = (id: EntityId): id is AllyId => id.startsWith('aly');
 export const isObjectId = (id: EntityId): id is ObjectId => id.startsWith('obj');
 export const isSkillId = (id: EntityId): id is SkillId => id.startsWith('skl');
 export const isTraitId = (id: EntityId): id is TraitId => id.startsWith('trt');
